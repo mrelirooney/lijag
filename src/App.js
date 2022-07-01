db2e268121536a50bd8e2f99bde77405bdf62491
Mr. EliRooney <mrelirooney@gmail.com>
2022-07-01 12:46:38 -0400

    Added a Button, I'll see if I can do math later

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='LvlSection'>
-        <p id="currentLvl">17</p>
-        <svg id="LvlMeter" width="100%" height="20">
-          <rect width="100%" height="20" fill="#ccc" rx="2" ry="2"></rect>
-          <rect width="50%" height="20" fill="#0078bc" rx="2" ry="2"></rect>
-        </svg>  
-        <p id="nextLvl">18</p>
+          <p id="currentLvl">17</p>
+          <svg id="LvlMeter" width="100%" height="20">
+            <rect width="100%" height="20" fill="#ccc" rx="2" ry="2"></rect>
+            <rect width="50%" height="20" fill="#0078bc" rx="2" ry="2"></rect>
+          </svg>  
+          <p id="nextLvl">18</p>
+        </div>
+        <div className='buttonSection'>
+        <button onClick={Task}>Default</button>
         </div>
       </header>
       <body className="App-body">
@@ -21,7 +24,10 @@ function App() {
 
       </footer>
     </div>
+    
   );
 }
-
+function Task() {
+  alert('You clicked me!');
+}
 export default App;
