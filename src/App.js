f8bd346801c84781b067abc18aac33b8bfbb902a
Mr. EliRooney <mrelirooney@gmail.com>
2022-07-02 13:44:42 -0400

    Added 9 more buttons using React instead of regular HTML, I'll see if I can do math tomorrow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
 import './App.css';
+import { Button } from './components/Button';
+
+
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +16,37 @@ function App() {
           </svg>  
           <p id="nextLvl">18</p>
         </div>
+        <div>
+          <p>0</p>
+        </div>
         <div className='buttonSection'>
-        <button onClick={Task}>Default</button>
+          <Button onClick={() => {console.log("This is worth 100")}} type="button"
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">100</Button>
+          <Button onClick={() => {console.log("This is worth 200")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">200</Button>
+          <Button onClick={() => {console.log("This is worth 300")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">300</Button>
+          <Button onClick={() => {console.log("This is worth 400")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">400</Button>
+          <Button onClick={() => {console.log("This is worth 500")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">500</Button>
+          <Button onClick={() => {console.log("This is worth 600")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">600</Button>
+          <Button onClick={() => {console.log("This is worth 700")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">700</Button>
+          <Button onClick={() => {console.log("This is worth 800")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">800</Button>
+          <Button onClick={() => {console.log("This is worth 900")}} type="button" 
+          buttonStyle="btn--warning--outline"
+          buttonSize="btn--large">900</Button>
         </div>
       </header>
       <body className="App-body">
@@ -27,7 +59,5 @@ function App() {
     
   );
 }
-function Task() {
-  alert('You clicked me!');
-}
+
 export default App;
