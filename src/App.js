86ddceaa8c233a6c9361935607e63a03af1a977c
Mr. EliRooney <mrelirooney@gmail.com>
2022-07-03 15:11:10 -0400

    Ok I can do some math and show a total on in the header...Went from 9 buttons to 3 buttons too to make the coding a problem solving easier

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button } from './components/Button';
 
 
 function App() {
-  
+  let totalValue = 200 + 400;
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +17,7 @@ function App() {
           <p id="nextLvl">18</p>
         </div>
         <div>
-          <p>0</p>
+          <p>{totalValue}</p>
         </div>
         <div className='buttonSection'>
           <Button onClick={() => {console.log("This is worth 100")}} type="button"
@@ -29,24 +29,7 @@ function App() {
           <Button onClick={() => {console.log("This is worth 300")}} type="button" 
           buttonStyle="btn--warning--outline"
           buttonSize="btn--large">300</Button>
-          <Button onClick={() => {console.log("This is worth 400")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">400</Button>
-          <Button onClick={() => {console.log("This is worth 500")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">500</Button>
-          <Button onClick={() => {console.log("This is worth 600")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">600</Button>
-          <Button onClick={() => {console.log("This is worth 700")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">700</Button>
-          <Button onClick={() => {console.log("This is worth 800")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">800</Button>
-          <Button onClick={() => {console.log("This is worth 900")}} type="button" 
-          buttonStyle="btn--warning--outline"
-          buttonSize="btn--large">900</Button>
+          
         </div>
       </header>
       <body className="App-body">
